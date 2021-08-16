aa721584e76d216171e5ceebb3db51d6fa509644
Prajval H L <prajvalhl@gmail.com>
2021-08-16 13:07:26 +0530

    refactor: added working navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,50 @@
 import React from "react";
+import { useNav } from "../nav-context";
 
 export function Header() {
+  const { route, setRoute } = useNav();
+
   return (
     <div className="header">
       <h1 className="brand-name">All About Triangles</h1>
       <div>
         <ul className="list nav-items">
-          <li className="li-inline">is Triangle?</li>
-          <li className="li-inline">Quiz</li>
-          <li className="li-inline">Hypotenuse</li>
-          <li className="li-inline">Area of Triangle</li>
+          <li className="li-inline">
+            <button
+              className="nav-btn"
+              style={{ fontWeight: route === "isTriangle" && "bold" }}
+              onClick={() => setRoute("isTriangle")}
+            >
+              is Triangle?
+            </button>
+          </li>
+          <li className="li-inline">
+            <button
+              className="nav-btn"
+              style={{ fontWeight: route === "quiz" && "bold" }}
+              onClick={() => setRoute("quiz")}
+            >
+              Quiz
+            </button>
+          </li>
+          <li className="li-inline">
+            <button
+              className="nav-btn"
+              style={{ fontWeight: route === "hypotenuse" && "bold" }}
+              onClick={() => setRoute("hypotenuse")}
+            >
+              Hypotenuse
+            </button>
+          </li>
+          <li className="li-inline">
+            <button
+              className="nav-btn"
+              style={{ fontWeight: route === "area" && "bold" }}
+              onClick={() => setRoute("area")}
+            >
+              Area
+            </button>
+          </li>
         </ul>
       </div>
     </div>
