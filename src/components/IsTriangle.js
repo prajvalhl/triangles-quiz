498a33fa7cf3c3dceae9e246dce09e1ba8b15b99
Prajval H L <prajvalhl@gmail.com>
2021-08-16 14:58:31 +0530

    refactor: updated IsTriangle component

--- src/components/IsTriangle.js
+++ src/components/IsTriangle.js
@@ -2,8 +2,29 @@ import React from "react";
 
 export function IsTriangle() {
   return (
-    <div>
-      <h1>IsTriangle</h1>
+    <div className="isTriangle">
+      <h1>Does it Make a Triangle?</h1>
+      <h3>Let's find out!</h3>
+      <p>
+        Fill the three angles below and we'll tell you if it makes a triangle or
+        not.
+      </p>
+      <div className="angle-input">
+        <label htmlFor="angle1">Angle 1</label>
+        <br />
+        <input type="number" id="angle1" />
+      </div>
+      <div className="angle-input">
+        <label htmlFor="angle2">Angle 2</label>
+        <br />
+        <input type="number" id="angle2" />
+      </div>
+      <div className="angle-input">
+        <label htmlFor="angle3">Angle 3</label>
+        <br />
+        <input type="number" id="angle3" />
+      </div>
+      <button className="btn-submit">Is Triangle?</button>
     </div>
   );
 }
