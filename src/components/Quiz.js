7d19c44d4fa2a213712eeaef5b18fcbecec28c0f
Prajval H L <prajvalhl@gmail.com>
2021-08-16 17:54:51 +0530

    refactor: updated quiz data

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { quizData } from "../quiz-data";
 
 export function Quiz() {
+  const [totalScore, setTotalScore] = useState(0);
+  const quizComponents = quizData.map((quiz) => (
+    <div key={quiz.id}>
+      <p></p>
+    </div>
+  ));
   return (
     <div>
-      <h1>Quiz</h1>
+      <h1>Quiz on Triangles</h1>
+      <h3>You'll gain one point for each correct answer</h3>
+      <h2>Under Development ⚠️.</h2>
     </div>
   );
 }
---
+++ src/quiz-data.js
@@ -0,0 +1,11 @@
+export const quizData = [
+  {
+    id: 1,
+    question:
+      "What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?",
+    option1: "45°",
+    option2: "90°",
+    option3: "60°",
+    correctAnswer: "90°"
+  }
+];
