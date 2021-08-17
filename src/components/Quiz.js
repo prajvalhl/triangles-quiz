3146095ec772c3867653c270eaa77dcc0ce9ca9b
Prajval H L <prajvalhl@gmail.com>
2021-08-17 11:54:49 +0530

    refactor: updated components

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,16 +3,73 @@ import { quizData } from "../quiz-data";
 
 export function Quiz() {
   const [totalScore, setTotalScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
   const quizComponents = quizData.map((quiz) => (
-    <div key={quiz.id}>
-      <p></p>
+    <div key={quiz.id} className="quiz">
+      <p>{quiz.question}</p>
+      <div>
+        <input
+          type="radio"
+          value={quiz.option1}
+          name={quiz.id}
+          onChange={(e) => {
+            if (e.target.value === quiz.correctAnswer) {
+              setTotalScore((prevScore) => prevScore + 1);
+            } else {
+              setTotalScore((prevScore) => prevScore + 0);
+            }
+          }}
+        />{" "}
+        {quiz.option1}
+        <input
+          type="radio"
+          value={quiz.option2}
+          name={quiz.id}
+          onChange={(e) => {
+            if (e.target.value === quiz.correctAnswer) {
+              setTotalScore((prevScore) => prevScore + 1);
+            } else {
+              setTotalScore((prevScore) => prevScore + 0);
+            }
+          }}
+        />{" "}
+        {quiz.option2}
+        <input
+          type="radio"
+          value={quiz.option3}
+          name={quiz.id}
+          onChange={(e) => {
+            if (e.target.value === quiz.correctAnswer) {
+              setTotalScore((prevScore) => prevScore + 1);
+            } else {
+              setTotalScore((prevScore) => prevScore + 0);
+            }
+          }}
+        />{" "}
+        {quiz.option3}
+      </div>
     </div>
   ));
   return (
     <div>
       <h1>Quiz on Triangles</h1>
       <h3>You'll gain one point for each correct answer</h3>
-      <h2>Under Development ⚠️.</h2>
+      {quizComponents}
+      <button
+        className="btn-submit"
+        onClick={() => {
+          setShowScore(true);
+        }}
+      >
+        Submit
+      </button>
+      <div className="div-output">
+        <p>Result 👇</p>
+        <p style={{ display: showScore ? "block" : "none" }}>
+          You Scored: {totalScore}
+        </p>
+      </div>
     </div>
   );
 }
--- src/quiz-data.js
+++ src/quiz-data.js
@@ -7,5 +7,81 @@ export const quizData = [
     option2: "90°",
     option3: "60°",
     correctAnswer: "90°"
+  },
+  {
+    id: 2,
+    question:
+      "What is the third angle for the triangle where angle1 = 45° and angle2 = 45°?",
+    option1: "obtuse",
+    option2: "acute",
+    option3: "right angled",
+    correctAnswer: "right angled"
+  },
+  {
+    id: 3,
+    question: "A triangle can have",
+    option1: "one right angle",
+    option2: "two right angle",
+    option3: "three right angle",
+    correctAnswer: "one right angle"
+  },
+  {
+    id: 4,
+    question: "Which of the following can form a right angled triangle?",
+    option1: "14, 15, 26",
+    option2: "12, 16, 20",
+    option3: "16, 18, 20",
+    correctAnswer: "12, 16, 20"
+  },
+  {
+    id: 5,
+    question: "Which of the following triangles are always similar?",
+    option1: "equilateral triangle",
+    option2: "isosceles triangle",
+    option3: "scalene Triangle",
+    correctAnswer: "equilateral triangle"
+  },
+  {
+    id: 6,
+    question:
+      "If one angle of a triangle is obtuse, then which one of the following is the possible measure of remaining angles?",
+    option1: "100°",
+    option2: "85°",
+    option3: "90°",
+    correctAnswer: "100°"
+  },
+  {
+    id: 7,
+    question:
+      "If the largest angle in a triangle is 70°, what is the least possible value of the smallest angle of the triangle?",
+    option1: "30°",
+    option2: "10°",
+    option3: "40°",
+    correctAnswer: "30°"
+  },
+  {
+    id: 8,
+    question: "The perimeter of scalene triangle with sides a, b, c is",
+    option1: "a + b + c",
+    option2: "2a",
+    option3: "None of these",
+    correctAnswer: "a + b + c"
+  },
+  {
+    id: 9,
+    question: "A scalene triangle has ___ lines of symmetry",
+    option1: "two",
+    option2: "no",
+    option3: "15",
+    correctAnswer: "no"
+  },
+  {
+    id: 10,
+    question:
+      "There is a right triangle PQR where: angle Q = 90°; angle P = angle R. What is the measure of angles P and R?",
+    option1: "85°",
+    option2: "65°",
+    option3: "45°",
+    correctAnswer: "45°"
   }
 ];
